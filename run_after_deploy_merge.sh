5cb08c75e306bceb8d74e028678f6855fb9bfc3f
ap-- <andreas@poehlmann.io>
2017-05-13 12:17:12 +0200

    fix script

--- run_after_deploy_merge.sh
+++ run_after_deploy_merge.sh
@@ -6,11 +6,13 @@ SBVERSION=$(git rev-parse --abbrev-ref HEAD)
 SeaBreeze/os-support/windows/scripts/update-visual-studio-projects
 
 # make new commit
-git add SeaBreeze/os-support/windows/VisualStudio2005/VCProj/*
-git add SeaBreeze/os-support/windows/VisualStudio2015/VCProj/*
+
+git add SeaBreeze/os-support/windows/VisualStudio2005/VSProj/SeaBreeze.vcproj
+git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj
+git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj.filters
 git commit -m "Hotfix visualstudio project files"
 
-git tag -a libseabreeze-${SBVERSION}
+git tag -a libseabreeze-${SBVERSION} -m 'new libseabreeze version'
 
 git push origin ${SBVERSION}
 git push origin libseabreeze-${SBVERSION}
