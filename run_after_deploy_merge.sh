d7a33b11f4f8f882306ee7aab5c95d94c3ce0ec6
ap-- <andreas@poehlmann.io>
2017-05-13 18:21:01 +0200

    commit patches

--- run_after_deploy_merge.sh
+++ run_after_deploy_merge.sh
@@ -5,10 +5,10 @@ SBVERSION=$(git rev-parse --abbrev-ref HEAD)
 SBPATH="SeaBreeze/os-support/windows"
 
 echo "Patching vc9 compatibility"
-git apply < 0001-vc9-if-false-is-incompatible.-Fatal-Error-C1017.patch
-git apply < 0002-vc9-vector.data-not-supported.patch
-git apply < 0003-vc9-Fix-snprintf-not-defined.patch
-git apply < 0004-Providing-VisualStudio2008-sln-for-python2.7-builds.patch
+git am --ignore-whitespace --signoff 0001-vc9-if-false-is-incompatible.-Fatal-Error-C1017.patch
+git am --ignore-whitespace --signoff 0002-vc9-vector.data-not-supported.patch
+git am --ignore-whitespace --signoff 0003-vc9-Fix-snprintf-not-defined.patch
+git am --ignore-whitespace --signoff 0004-Providing-VisualStudio2008-sln-for-python2.7-builds.patch
 
 # Add all files to project
 echo "Updating project resources"
@@ -25,5 +25,5 @@ echo "tag"
 git tag -a libseabreeze-${SBVERSION} -m 'new libseabreeze version'
 
 echo "push"
-git push origin ${SBVERSION}
-git push origin libseabreeze-${SBVERSION}
+echo "git push origin ${SBVERSION}"
+echo "git push origin libseabreeze-${SBVERSION}"
