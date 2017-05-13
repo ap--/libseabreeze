baef3f0ab576d132cc55876838c7f8462635119a
ap-- <andreas@poehlmann.io>
2017-05-13 13:20:28 +0200

    trying vs2010 > vs2008 conversion trick

--- run_after_deploy_merge.sh
+++ run_after_deploy_merge.sh
@@ -2,12 +2,19 @@
 
 SBVERSION=$(git rev-parse --abbrev-ref HEAD)
 
+SBPATH="SeaBreeze/os-support/windows"
+
 # Add all files to project
 SeaBreeze/os-support/windows/scripts/update-visual-studio-projects
 
-# make new commit
+cp ${SBPATH}/VisualStudio2010/SeaBreeze.sln ./SB.sln
+sed '1s/11.0/10.0/' ./SB.sln > ${SBPATH}/VisualStudio2010/SeaBreeze.sln
 
+# make new commit
 git add SeaBreeze/os-support/windows/VisualStudio2005/VSProj/SeaBreeze.vcproj
+git add SeaBreeze/os-support/windows/VisualStudio2010/SeaBreeze.sln
+git add SeaBreeze/os-support/windows/VisualStudio2010/VSProj/SeaBreeze.vcxproj
+git add SeaBreeze/os-support/windows/VisualStudio2010/VSProj/SeaBreeze.vcxproj.filters
 git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj
 git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj.filters
 git commit -m "Hotfix visualstudio project files"
