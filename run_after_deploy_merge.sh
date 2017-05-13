70a7c18ba823ae4de57192b98518efc47d4f9063
ap-- <andreas@poehlmann.io>
2017-05-13 18:04:01 +0200

    Updating scripts

--- run_after_deploy_merge.sh
+++ run_after_deploy_merge.sh
@@ -4,22 +4,26 @@ SBVERSION=$(git rev-parse --abbrev-ref HEAD)
 
 SBPATH="SeaBreeze/os-support/windows"
 
+echo "Patching vc9 compatibility"
+git apply < 0001-vc9-if-false-is-incompatible.-Fatal-Error-C1017.patch
+git apply < 0002-vc9-vector.data-not-supported.patch
+git apply < 0003-vc9-Fix-snprintf-not-defined.patch
+git apply < 0004-Providing-VisualStudio2008-sln-for-python2.7-builds.patch
+
 # Add all files to project
+echo "Updating project resources"
 SeaBreeze/os-support/windows/scripts/update-visual-studio-projects
 
-cp ${SBPATH}/VisualStudio2010/SeaBreeze.sln ./SB.sln
-sed '1s/11.0/10.0/' ./SB.sln > ${SBPATH}/VisualStudio2010/SeaBreeze.sln
-
 # make new commit
-git add SeaBreeze/os-support/windows/VisualStudio2005/VSProj/SeaBreeze.vcproj
-git add SeaBreeze/os-support/windows/VisualStudio2010/SeaBreeze.sln
-git add SeaBreeze/os-support/windows/VisualStudio2010/VSProj/SeaBreeze.vcxproj
-git add SeaBreeze/os-support/windows/VisualStudio2010/VSProj/SeaBreeze.vcxproj.filters
+echo "commiting updated resources"
+git add SeaBreeze/os-support/windows/VisualStudio2008/VSProj/SeaBreeze.vcproj
 git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj
 git add SeaBreeze/os-support/windows/VisualStudio2015/VSProj/SeaBreeze.vcxproj.filters
 git commit -m "Hotfix visualstudio project files"
 
+echo "tag"
 git tag -a libseabreeze-${SBVERSION} -m 'new libseabreeze version'
 
+echo "push"
 git push origin ${SBVERSION}
 git push origin libseabreeze-${SBVERSION}
